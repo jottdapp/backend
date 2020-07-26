c43e3fba0f2df0a06e661a7b0008ddbfc1658b3b
Maya Farber Brodsky <mayaf2003@gmail.com>
2020-07-26 21:54:27 +0300

    add files

---
+++ main.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from routers import auth
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"hello": "world"}
+
+
+app.include_router(auth.router, prefix="/api/auth")
---
+++ routers/auth.py
@@ -0,0 +1,62 @@
+from fastapi import APIRouter, Cookie
+from fastapi.responses import JSONResponse
+import hashlib
+from passlib.context import CryptContext
+from deta import Deta
+from pydantic import BaseModel
+from uuid import uuid4
+import os
+
+router = APIRouter()
+
+deta = Deta(os.getenv("DETA_BASE_KEY"))
+users = deta.Base("users")
+
+
+class SignupData(BaseModel):
+    username: str
+    password: str
+
+
+def login_info(session):
+    data = sessions.get(session)
+    return data["username"]
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+@router.post("/signup")
+async def signup(data: SignupData):
+    if users.get(data.username):
+        return {"error": "Username already in use."}
+
+    users.put({"password": get_password_hash(data.password)}, data.username)
+    return {"success": True}
+
+
+class LoginData(BaseModel):
+    username: str
+    password: str
+
+
+@router.post("/login")
+async def login(data: LoginData):
+    entry = users.get(data.username)
+    if not entry:
+        return {"error": "Unknown username."}
+    elif not verify_password(data.password, entry["password"]):
+        return {"error": "Wrong password."}
+    else:
+        session_cookie = str(uuid4())
+        response = JSONResponse(content={"success": True})
+        response.set_cookie(key="session", value=session_cookie)
+        return response
