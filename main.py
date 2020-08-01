c93300a9aaa0900dce06c6f0d0a88ca5cd288ef1
Misha Farber Brodsky <taco@mailo.com>
2020-08-01 13:22:05 +0300

    Add CORS with environment variable ORIGINS

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from routers import auth
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"hello": "world"}
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=os.getenv("ORIGINS").split(" "),
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+)
 
 app.include_router(auth.router, prefix="/api/auth")
